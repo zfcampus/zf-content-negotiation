35da884551aab472e821a98b59bb9693c62c9990
Matthew Weier O'Phinney <matthew@zend.com>
2013-08-02 17:08:17 -0500

    Refactored AcceptListener
    - Broke logic for both retrieving selector criteria and populating view
      model into separate methods
    - Composes module configuration and AcceptableViewModelSelector
      internally to simplify logic
    - Updated Module to add factory for AcceptListener to inject
      AcceptableViewModelSelector and module configuration
    - ViewModel population now:
      - does nothing if the view model class matches the class of a view
        model result
      - injects both the variables and template from a view model result
        when "re-casting" a view model
      - injects non-view model, non-array results as "result" variable of
        selected view model; will require a change to ZF\Hal
    - No longer sets the view model as "terminal". This will require either (a)
      individual view model implementations to set this properly, and/or (b)
      developers to register a listener to set it.

--- src/ZF/ContentNegotiation/AcceptListener.php
+++ src/ZF/ContentNegotiation/AcceptListener.php
@@ -2,69 +2,142 @@
 
 namespace ZF\ContentNegotiation;
 
+use Zend\Mvc\Controller\Plugin\AcceptableViewModelSelector;
 use Zend\Mvc\MvcEvent;
+use Zend\View\Model\ModelInterface as ViewModelInterface;
 
 class AcceptListener
 {
+    /**
+     * @var AcceptableViewModelSelector
+     */
+    protected $selector;
+
+    /**
+     * @var array
+     */
+    protected $controllerConfig = array();
+
+    /**
+     * @var array
+     */
+    protected $selectorsConfig = array();
+
+    /**
+     * @param AcceptableViewModelSelector $selector 
+     * @param array $config 
+     */
+    public function __construct(AcceptableViewModelSelector $selector, array $config)
+    {
+        $this->selector = $selector;
+
+        if (isset($config['controllers'])
+            && is_array($config['controllers'])
+        ) {
+            $this->controllerConfig = $config['controllers'];
+        }
+
+        if (isset($config['selectors'])
+            && is_array($config['selectors'])
+        ) {
+            $this->selectorsConfig = $config['selectors'];
+        }
+    }
+
+    /**
+     * @param  MvcEvent $e 
+     */
     public function __invoke(MvcEvent $e)
     {
-        $app = $e->getApplication();
+        $criteria = $e->getParam('ZFContentNegotiation');
 
-        $controller = $e->getTarget();
-        $controllerConfig = $e->getParam('ZFContentNegotiation');
-
-        if (!$controllerConfig) {
-            $services = $app->getServiceManager();
-            $appConfig = $services->get('Config');
-
-
-            if (!isset($appConfig['zf-content-negotiation']) || !isset($appConfig['zf-content-negotiation']['controllers'])) {
-                goto FALLBACK;
-            }
-
-            // get the controllers from the content-neg configuration
-            $controllers = $appConfig['zf-content-negotiation']['controllers'];
+        // If we have no criteria, derive it from configuration and/or any set fallbacks
+        if (!$criteria) {
+            $fallbackConfig = $e->getParam('ZFContentNegotiationFallback');
             $controllerName = $e->getRouteMatch()->getParam('controller');
 
-            // if there is no config for this controller, move on
-            if (!$controllerName || !isset($controllers[$controllerName])) {
-                goto FALLBACK;
-            }
+            $criteria = $this->getSelectorCriteria($fallbackConfig, $controllerName);
+        }
 
-            // if its an array, that means its direct configuration
-            if (is_array($controllers[$controllerName])) {
-                $controllerConfig = $controllers[$controllerName];
-            } elseif (is_string($controllers[$controllerName])) {
+        // Retrieve a view model based on the criteria
+        $selector  = $this->selector;
+        $viewModel = $selector($criteria);
 
-                // if its a string, we should try to resolve that key to a rusable selector set
-                if (isset($appConfig['zf-content-negotiation']['selectors'])) {
-                    $selectors = $appConfig['zf-content-negotiation']['selectors'];
-                    if (isset($selectors[$controllers[$controllerName]])) {
-                        $controllerConfig = $selectors[$controllers[$controllerName]];
-                    }
+        // Populate the view model with the result...
+        $this->populateViewModel($result, $viewModel);
+    }
+
+    /**
+     * Derive the view model selector criteria
+     *
+     * Try and determine the view model selection criteria based on the configuration
+     * for the current controller service name, using a fallback if it exists.
+     * 
+     * @param  null|array $fallbackConfig 
+     * @param  string $controllerName 
+     * @return null|array
+     */
+    protected function getSelectorCriteria($fallbackConfig, $controllerName)
+    {
+        $criteria = null;
+
+        if (empty($this->controllerConfig)) {
+            return $fallbackConfig;
+        }
+
+        // get the controllers from the content-neg configuration
+        $controllers = $this->controllerConfig;
+
+        // if there is no config for this controller, move on
+        if (!$controllerName || !isset($controllers[$controllerName])) {
+            return $fallbackConfig;
+        }
+
+        $criteria = $controllers[$controllerName];
+
+        // if it's an array, that means we have direct configuration
+        if (is_array($criteria)) {
+            return $criteria;
+        }
+
+        // if it's a string, we should try to resolve that key to a reusable selector set
+        if (is_string($criteria)) {
+            if (isset($this->selectorsConfig[$criteria])) {
+                $criteria = $this->selectorsConfig[$criteria];
+                if (!empty($criteria)) {
+                    return $criteria;
                 }
-
+                return $fallbackConfig;
             }
         }
 
-        FALLBACK:
+        return $fallbackConfig;
+    }
 
-        if (!$controllerConfig) {
-            $controllerConfig = $e->getParam('ZFContentNegotiationFallback');
-        }
-
-        if ($controllerConfig) {
-            $result = $e->getResult();
-            /** @var \Zend\View\Model\ViewModel $viewModel */
-            $viewModel = $controller->acceptableViewModelSelector($controllerConfig);
-            if (is_array($result)) {
-                $viewModel->setVariables($result);
-            } elseif ($result instanceof ViewModel) {
-                $viewModel->setVariables($result->getVariables());
+    protected function populateViewModel($result, ViewModelInterface $viewModel)
+    {
+        if ($result instanceof ViewModelInterface) {
+            // If the result is of the same type as the selected view model, do nothing
+            if (get_class($result) == get_class($viewModel)) {
+                return;
             }
 
-            $viewModel->setTerminal(true);
+            // Otherwise, "re-cast" the view model
+            $viewModel->setVariables($result->getVariables());
+            $viewModel->setTemplate($result->getTemplate());
             $e->setResult($viewModel);
+            return;
         }
+
+        // If result is an array, use it to populate the view model variables
+        if (is_array($result)) {
+            $viewModel->setVariables($result);
+            $e->setResult($viewModel);
+            return;
+        }
+
+        // For all other result types, set the "result" variable to the result value
+        $viewModel->setVariable('result', $result);
+        $e->setResult($viewModel);
     }
 }
