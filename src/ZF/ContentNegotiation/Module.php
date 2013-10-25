1a38d0c4f6a5c2d834bd773569f21bea3deb5232
Michaël Gallego <mic.gallego@gmail.com>
2013-10-25 19:08:22 +0200

    Cleanup

--- src/ZF/ContentNegotiation/Module.php
+++ src/ZF/ContentNegotiation/Module.php
@@ -11,6 +11,9 @@ use Zend\Mvc\MvcEvent;
 
 class Module
 {
+    /**
+     * {@inheritDoc}
+     */
     public function getAutoloaderConfig()
     {
         return array(
@@ -22,94 +25,17 @@ class Module
         );
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getConfig()
     {
         return include __DIR__ . '/../../../config/module.config.php';
     }
 
-    public function getServiceConfig()
-    {
-        return array('factories' => array(
-            'ZF\ContentNegotiation\AcceptListener' => function ($services) {
-                $config = array();
-                if ($services->has('Config')) {
-                    $appConfig = $services->get('Config');
-                    if (isset($appConfig['zf-content-negotiation'])
-                        && is_array($appConfig['zf-content-negotiation'])
-                    ) {
-                        $config = $appConfig['zf-content-negotiation'];
-                    }
-                }
-
-                $selector = null;
-                if ($services->has('ControllerPluginManager')) {
-                    $plugins = $services->get('ControllerPluginManager');
-                    if ($plugins->has('AcceptableViewModelSelector')) {
-                        $selector = $plugins->get('AcceptableViewModelSelector');
-                    }
-                }
-                if (null === $selector) {
-                    $selector = new AcceptableViewModelSelector();
-                }
-                return new AcceptListener($selector, $config);
-            },
-            'ZF\ContentNegotiation\AcceptFilterListener' => function ($services) {
-                $listener = new AcceptFilterListener();
-
-                $config   = array();
-                if ($services->has('Config')) {
-                    $moduleConfig = false;
-                    $appConfig    = $services->get('Config');
-                    if (isset($appConfig['zf-content-negotiation'])
-                        && is_array($appConfig['zf-content-negotiation'])
-                    ) {
-                        $moduleConfig = $appConfig['zf-content-negotiation'];
-                    }
-
-                    if ($moduleConfig
-                        && isset($moduleConfig['accept-whitelist'])
-                        && is_array($moduleConfig['accept-whitelist'])
-                    ) {
-                        $config = $moduleConfig['accept-whitelist'];
-                    }
-                }
-
-                if (!empty($config)) {
-                    $listener->setConfig($config);
-                }
-
-                return $listener;
-            },
-            'ZF\ContentNegotiation\ContentTypeFilterListener' => function ($services) {
-                $listener = new ContentTypeFilterListener();
-
-                $config   = array();
-                if ($services->has('Config')) {
-                    $moduleConfig = false;
-                    $appConfig    = $services->get('Config');
-                    if (isset($appConfig['zf-content-negotiation'])
-                        && is_array($appConfig['zf-content-negotiation'])
-                    ) {
-                        $moduleConfig = $appConfig['zf-content-negotiation'];
-                    }
-
-                    if ($moduleConfig
-                        && isset($moduleConfig['content-type-whitelist'])
-                        && is_array($moduleConfig['content-type-whitelist'])
-                    ) {
-                        $config = $moduleConfig['content-type-whitelist'];
-                    }
-                }
-
-                if (!empty($config)) {
-                    $listener->setConfig($config);
-                }
-
-                return $listener;
-            },
-        ));
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     public function onBootstrap($e)
     {
         $app      = $e->getApplication();
